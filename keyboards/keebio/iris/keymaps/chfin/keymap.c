7a247fa9ac7cc9ce05dd029e4dd053d7a94d95f9
Christoph Finkensiep <chfin@chfin.de>
2020-03-20 11:17:30 +0100

    some improvements to the iris layout

--- keyboards/keebio/iris/keymaps/chfin/keymap.c
+++ keyboards/keebio/iris/keymaps/chfin/keymap.c
@@ -23,23 +23,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────────────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT,             KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────────────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,          KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_LCTL,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,          KC_RALT,  KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ENT),
   //└────────────────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                                KC_LALT, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   KC_LGUI
+                                                KC_LALT, LOWER,   KC_SPC,                    KC_SPC,  RAISE,   KC_LGUI
                                             // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                              ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
+     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                              ├────────┼────────┼────────┼────────┼────────┼────────┤
      RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGDN,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                              ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, KC_LEFT, KC_RGHT, KC_UP,   KC_LBRC,                                KC_RBRC, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_HOME,
+     _______, KC_NO,   KC_PGUP, KC_UP,   KC_PGDN, KC_LBRC,                                KC_RBRC, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_HOME,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐            ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, KC_DOWN, KC_LCBR, KC_MEDIA_PLAY_PAUSE,  KC_RPRN, KC_RCBR, RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, KC_END,
+     _______, KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_LCBR, KC_MEDIA_PLAY_PAUSE,  KC_DEL,  KC_RCBR, RGB_RMOD,RGB_HUD, RGB_SAD, RGB_VAD, KC_END,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘            └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, KC_DEL,                        KC_DEL,  _______, KC_P0
+                                    _______, _______, KC_ENT,                        KC_ENT,  _______, KC_LGUI
                                 // └────────┴────────┴────────┘                     └────────┴────────┴────────┘
   ),
 
@@ -47,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+     RGB_TOG, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, KC_UNDS,                            KC_EQL,  KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT,_______,
+     _______, KC_MPRV, KC_MNXT, KC_VOLU, KC_HOME, KC_UNDS,                            KC_EQL,  KC_PGUP, KC_UP,   KC_PGDN, KC_PLUS, KC_INS,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_MINS, KC_LPRN,          _______, KC_PLUS, KC_P1,   KC_P2,   KC_PLUS, KC_MINS, _______,
+     _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_END,  KC_MINS, KC_LPRN,          KC_LPRN, KC_PLUS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_MINS, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├───────────┼───────────┼───────────┼───────────┼───────────┼───────────┼─── ─────┐   ┌─────────┼───────────┼───────────┼──────────────┼─────────────┼──────────────┼──────────────┤
      KC_LCTL,    RGUI(KC_Z), RGUI(KC_X), RGUI(KC_C), RGUI(KC_V), RGUI(KC_B),  _______,     _______,  RGUI(KC_N), RGUI(KC_M), RGUI(KC_COMM), RGUI(KC_DOT), RGUI(KC_SLSH), KC_RSFT,
   //└───────────┴───────────┴───────────┴─────┬─────┴─────┬─────┴─────┬─────┴────┬────┘   └───┬─────┴─────┬─────┴─────┬─────┴────┬─────────┴─────────────┴──────────────┴──────────────┘
-                                               KC_LGUI,    _______,    RGUI(KC_ENT),           RGUI(KC_SPC), _______,  KC_RALT
+                                               _______,    _______,    RGUI(KC_ENT),           RGUI(KC_ENT), _______,  _______
                                            // └───────────┴───────────┴────────────┘          └───────────┴───────────┴──────────┘
   )
 };
